79e13a0ab8a83ac923a3855bdaa64fb8e4551436
sssv587 <1092663295@qq.com>
2022-07-13 00:45:22 +0800

    IOC - Bean管理注解方式(属性注入Resource与Value)

--- spring5-demo3/src/com/futurebytedance/service/UserService.java
+++ spring5-demo3/src/com/futurebytedance/service/UserService.java
@@ -3,7 +3,10 @@ package com.futurebytedance.service;
 import com.futurebytedance.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * @author yuhang.sun
@@ -16,15 +19,22 @@ import org.springframework.stereotype.Component;
 //@Component(value = "userService") //<bean id="" class=".."/>
 @Component
 public class UserService {
+    @Value(value = "abc")
+    private String name;
+
     //定义dao类型属性
     //不需要添加set方法
     //添加注入属性注解
-    @Autowired //根据类型进行注入
-    @Qualifier(value = "userDaoImpl1") //根据名称进行注入,比如一个接口有多个实现类,需要指定名称
+//    @Autowired //根据类型进行注入
+//    @Qualifier(value = "userDaoImpl1") //根据名称进行注入,比如一个接口有多个实现类,需要指定名称
+//    private UserDao userDao;
+
+    //    @Resource //根据类型进行注入
+    @Resource(name = "userDaoImpl1") //根据名称进行注入
     private UserDao userDao;
 
     public void add() {
-        System.out.println("Service add...");
+        System.out.println("Service add..." + name);
         userDao.add();
     }
 }
