0bf9a49d7e2e3d9f83fa911a491245075beeb893
sssv587 <1092663295@qq.com>
2022-07-17 18:04:21 +0800

    事务-XML形式

--- spring5-txdemo1/src/com/futurebytedance/test/TestUser.java
+++ spring5-txdemo1/src/com/futurebytedance/test/TestUser.java
@@ -1,8 +1,10 @@
 package com.futurebytedance.test;
 
+import com.futurebytedance.config.TxConfig;
 import com.futurebytedance.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -15,6 +17,23 @@ public class TestUser {
     @Test
     public void testAccount() {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        userService.accountMoney();
+    }
+
+
+    //XML方式声明事务
+    @Test
+    public void testAccountXml() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        userService.accountMoney();
+    }
+
+    //注解方式声明事务
+    @Test
+    public void testAccountAnno() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(TxConfig.class);
         UserService userService = context.getBean("userService", UserService.class);
         userService.accountMoney();
     }
